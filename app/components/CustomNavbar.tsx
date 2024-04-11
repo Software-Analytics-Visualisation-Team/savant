0bd3e39c159c3e022f1fe2f891094ebb5e6b0760
Zamfirov <filip.zamfirov1996@gmail.com>
2024-04-11 10:47:38 +0200

    Basic NextUi navbar customised

--- app/components/CustomNavbar.tsx
+++ app/components/CustomNavbar.tsx
@@ -1,18 +1,14 @@
 import React from "react";
+import Image from "next/image";
 import {Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import SavantLogo from "../../public/savant_brain.jpeg";
 
 export default function CustomNavBar() {
   const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
+    "Home",
+    "Projects",
+    "Education",
+    "Contacts",
   ];
 
   return (
@@ -23,32 +19,42 @@ export default function CustomNavBar() {
 
       <NavbarContent className="sm:hidden pr-3" justify="center">
         <NavbarBrand>
-          <p className="font-bold text-inherit">ACME</p>
+          <Image src={SavantLogo} alt="SAVANT logo" width={42} height={42}/>
+          <p className="font-serif text-red-600">SAVANT</p>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarBrand>
-          <p className="font-bold text-inherit">ACME</p>
+          <Image src={SavantLogo} alt="SAVANT logo" width={42} height={42}/>
+          <p className="font-serif text-inherit text-red-600">SAVANT</p>
         </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="warning">
-            Customers
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem>
+          <Link color="foreground" href="#" aria-current="page">
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
           <Link color="foreground" href="#">
-            Integrations
+            Education
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contacts
           </Link>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">
+      {/* <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
         </NavbarItem>
@@ -57,16 +63,17 @@ export default function CustomNavBar() {
             Sign Up
           </Button>
         </NavbarItem>
-      </NavbarContent>
+      </NavbarContent> */}
 
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
               className="w-full"
-              color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
+              // color={
+              //   index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              // }
+              color="foreground"
               href="#"
               size="lg"
             >
