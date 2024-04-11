3d1b69d9f0ec13ac2340bf2ddb50a6149406bb73
Zamfirov <filip.zamfirov1996@gmail.com>
2024-04-11 09:42:18 +0200

    Try explicitly importing image to fix GitHub page issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import NextjsLogo from "../public/next.svg"
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src={NextjsLogo}
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
