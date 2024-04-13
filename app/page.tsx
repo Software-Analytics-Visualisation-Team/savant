eebcdaaee15e573867446ac7a9fc59f4a0a229b9
Zamfirov <filip.zamfirov1996@gmail.com>
2024-04-13 10:53:28 +0200

    Final fix of routing

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ import PersonCard from "./components/PersonCard";
 import { Navbar } from "./components/Navbar"
 
 const menuItems = [
-  {name: "Projects", route: "/projects"},
-  {name: "Positions", route: "/openings"},
-  {name: "Contact us", route: "/contacts"},
+  {name: "Projects", route: "./projects"},
+  {name: "Positions", route: "./openings"},
+  {name: "Contact us", route: "./contacts"},
 ];
 
 export default function Home() {
