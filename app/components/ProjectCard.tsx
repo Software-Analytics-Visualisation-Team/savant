400bf3928f5eb558da85e01ab14a91ad17579d50
Zamfirov <filip.zamfirov1996@gmail.com>
2024-04-19 20:13:06 +0200

    Fix layout of project titles in case of missing image

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -24,22 +24,25 @@ export default function ProjectCard(props: ProjectProps) {
                 <CardBody>
                 <div className="flex flex-col items-center justify-cente">
                     <div className="relative col-start-1 col-span-1 md:col-span-2">
-                        <div className="grid grid-cols-6 gap-2 items-center">
                             {props.imageSource != "" &&
-                                <Image
-                                    alt="Project image"
-                                    className="object-cover col-span-1"
-                                    height={100}
-                                    shadow="md"
-                                    src={props.imageSource}
-                                    width={100}
-                                />
-                            }
-                            
-                            <div className="col-start-2 col-span-5">
-                                <h3 className="font-semibold text-2xl text-foreground/90">{props.projectName}</h3>
+                                <div className="grid grid-cols-6 gap-2 items-center">
+                                    <Image
+                                        alt="Project image"
+                                        className="object-cover col-span-1"
+                                        height={100}
+                                        shadow="md"
+                                        src={props.imageSource}
+                                        width={100}
+                                    />
+                                    <div className="col-start-2 col-span-5">
+                                    <h3 className="font-semibold text-2xl text-foreground/90">{props.projectName}</h3>
+                                </div>
                             </div>
-                        </div>
+                            }
+
+                            {props.imageSource == "" &&
+                                <h3 className="font-semibold text-2xl text-foreground/90">{props.projectName}</h3>
+                            }
                         <div className="flex justify-center">
                         {props.projectDescription}
                         </div>
