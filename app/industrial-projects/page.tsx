d5777633b31fe82f8fef9a214a0613b601048223
Zamfirov <filip.zamfirov1996@gmail.com>
2024-06-14 14:33:13 +0200

    Draft of Industry Page

--- app/industrial-projects/page.tsx
+++ app/industrial-projects/page.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import { Link } from "@nextui-org/react";
+import ContactCard from "../components/ContactCard"
+import IndustryImage from "../../public/industry-image-3.jpg"
+import MichelPortrait from "../../public/portraits/michel.jpeg"
 
 const menuItems = [
   { name: "Industrial Projects", route: "./industrial-projects" },
@@ -11,49 +15,73 @@ const menuItems = [
   { name: "Contact us", route: "./contacts" },
 ];
 
-const jobs = [
+const tools = [
   {
-    name: "COMARIC (PhD Position)",
-    description: "Software Intensive Systems must meet rigorous quality and reliability standards while being agile in responding to business stakeholders. Developing such systems faces several challenges: complexity, scale, and legacy evolution. The key approach is architecture, which provides a high-level representation of the system, illustrating how it's hierarchically decomposed into subsystems and how these subsystems interact. The ultimate goal in this project is to create automated tools that maintain consistency between the architecture and the implementation",
-    applyLink: "mailto:m.r.v.chaudron@tue.nl?subject=COMARIC (PhD Position)",
+    key:"AP",
+    name: <Link className="hover:text-red-600" color="foreground" href="./projects/galaxy">
+    Andromeda Platform
+  </Link>,
+    description: 'Custom analysis of your source code based on your needs. This platform provides a easily customisable way to analyse various aspects of your systems. Visualise your software, identify performance bottlenecks, detect security vulnerabilities.',
   },
   {
-    name: "Cynergy4me (PhD Position)",
-    description: "The use of Low Code-approaches has proven very successful in developing business automation software. We believe the success of Low Code is the well engineered alignment of Low Code = DSL + MBSE + Reference Architecture + CI/CD. We are looking for a candidate that can develop a method and tool for showing the feasibility of low-code for embedded system - so practical knowledge of DSL's & MBSE, Code Generation and good understanding of Software Architecture.oin our team as a product manager and drive innovation.",
-    applyLink: "mailto:m.r.v.chaudron@tue.nl?subject=Cynergy4me (PhD Position)",
+    key:"ClassViz",
+    name: <Link className="hover:text-red-600" color="foreground" href="./projects/galaxy">
+    ClassViz
+  </Link>,
+    description: 'Explore interactivelly software architecture visualisations. Understand your software’s structure, dependencies, and hotspots at a glance.',
   },
   {
-    name: "Software Analytics Infrastructure (PhD Position) ",
-    description: "The ultimate goal is to create a platform for the building of workflows that connect various automated tools for the analysis of software artefacts.",
-    applyLink: "mailto:m.r.v.chaudron@tue.nl?subject=Software Analytics Infrastructure (PhD Position)",
+    key:"BubbleTea",
+    name: 'Layered Bubble Tea Software Architecture Visualization',
+    description: 'Profile application performance and optimize resource usage.',
   },
-  // Add more job positions as needed
 ];
 
-export default function JobsPage() {
+export default function IndustrialProjectsPage() {
   return (
-    <div>
+      <div>
       <Navbar menuItems={menuItems} pageSelected="" />
-      <div className="flex flex-col bg-gray-50 min-h-screen p-8">
-        <h1 className="text-3xl font-bold mb-4">Job Openings</h1>
-        <ul className="space-y-4">
-          {jobs.map((job, index) => (
-            <li key={index} className="bg-white p-4 rounded shadow">
-              <h2 className="text-lg font-semibold">{job.name}</h2>
-              <p className="text-gray-600">{job.description}</p>
-              <a
-                href={job.applyLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-red-600 hover:underline"
-              >
-                Apply Now
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-col items-center justify-center bg-gray-50 min-h-screen">
+      <img
+        src={IndustryImage.src} // Replace with your actual image URL
+        alt="Full-width image"
+        style={{ width: '100vw', height: 'auto' }}
+      />
+          <div className="max-w-5xl">
+              <h1 className="text-3xl font-bold text-red-600 mb-4 pt-6">Unlocking Software Insights: Your Path to Better Architecture</h1>
+
+              <div className="full-width-image">
+
+    </div>
+              <p className="text-gray-600 mb-4">
+              Is your company or your team grappling with complex software systems? Struggling to decipher intricate architectures? We are experienced in working on practical challenges, and are activelly collaborating with industrial parterns and helping them solve complex problems conserning software analysis and architecture visualization.
+              </p>
+              <h1 className="text-2xl font-bold text-red-700 mb-4">What We Offer</h1>
+              <p className="text-gray-600 mb-4">
+              We provide state-of-the-art tools to analyse and visualise your software. Our tools:
+              </p>
+                <ul>
+                  {tools.map((tool) => (
+                    <li key={tool.key} className="pb-1">
+                      <strong>{tool.name}:</strong> {tool.description}
+                    </li>
+                  ))}
+                </ul>
+              <p className="text-gray-600 mb-4">
+              </p>
+              <h1 className="text-2xl font-bold text-red-700 mb-4">Contact Us</h1>
+              {/* <p>Reach out to us! We are actively looking for challenging problems.</p> */}
+              <ContactCard
+                name="Michel R.V. Chaudron"
+                position="Full Professor"
+                image={MichelPortrait.src}
+                email="m.r.v.chaudron@tue.nl"
+                personalPage="https://research.tue.nl/en/persons/michel-rv-chaudron"
+              />
+          </div>
+
       </div>
       <Footer />
-    </div>
+  </div>
   );
 };
