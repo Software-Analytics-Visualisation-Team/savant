b1bdd274cbb25525aa9fab6d52962a7f275a70aa
Felipe Xavier <felipeacx@gmail.com>
2024-10-17 17:31:27 +0200

    Tried to remove the need for commenting out line next.config.mjs when running in dev mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/savant",  // <=== comment out before running npm run dev
+    basePath: (process.env.NODE_ENV === "development" ? "" : "/savant"),  // <=== comment out before running npm run dev
     output: "export",  // <=== enables static exports
     images: { unoptimized: true },
     reactStrictMode: true
