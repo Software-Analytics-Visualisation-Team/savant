678773f07a000cd2b0a95fb7bdff2a3a4c717263
Bouwmeester, Kevin <k.bouwmeester@tue.nl>
2025-11-10 11:06:52 +0100

    Refactor personcards --> Allow reuse

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import JacobPortrait from '../public/portraits/jacob.jpg';
 import SupunPortrait from '../public/portraits/Supun.png';
 import { NextUIProvider } from '@nextui-org/react';
 import PersonCard from './components/PersonCard';
+import { FelipeCard, FilipCard, JacobCard, KevinCard, LinaCard, MerelCard, MichelCard, SatrioCard, SupunCard } from './personcards';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { MENU_ITEMS as menuItems } from './constants';
@@ -73,69 +74,15 @@ export default function Home() {
             Our Team
           </div>
           <div className="flex flex-row flex-wrap gap-2 justify-center px-8 pb-8 mx-auto">
-            <PersonCard
-              name="Michel R.V. Chaudron"
-              position="Full Professor"
-              image={MichelPortrait.src}
-              quote="Research interests: Software Architecture, Software Design, Software Modelling with a special focus on UML, Software Composition and knowledge sharing, use of AI (Artificial Intelligence) for Software Development."
-              personalPage="https://research.tue.nl/en/persons/michel-rv-chaudron"
-            />
-            <PersonCard
-              name="Lina Ochoa Venegas"
-              position="Assistant Professor"
-              image={LinaPortrait.src}
-              quote="I am interested on studying the phenomena behind software evolution, maintenance, and analysis, and providing tools that support related processes."
-              personalPage="https://research.tue.nl/en/persons/lina-ochoa-venegas"
-            />
-            <PersonCard
-              name="Jacob Krüger"
-              position="Assistant Professor"
-              image={JacobPortrait.src}
-              quote="I am specializing in the development and evolution of variant-rich software systems. Variant-rich systems exist in different customizable variants and have become the most widely existing form of software systems in all areas of society."
-              personalPage="https://research.tue.nl/en/persons/jacob-kr%C3%BCger"
-            />
-            <PersonCard
-              name="Satrio Adi Rukmono"
-              position="PhD Candidate"
-              image={SatrioPortrait.src}
-              quote="Research interests: Application of Large Language Models (LLMs) in the field of Software Architecture, Software Visualisation"
-              personalPage="https://research.tue.nl/en/persons/satrio-rukmono"
-            />
-            <PersonCard
-              name="Filip Zamfirov"
-              position="PhD Candidate"
-              image={FilipPortrait.src}
-              quote="Research interests: Software Architecture Erosion, Software Visualisation, Static & Dynamic Analysis"
-              personalPage="https://research.tue.nl/en/persons/filip-zamfirov"
-            />
-            <PersonCard
-              name="Merel Veracx"
-              position="PhD Candidate"
-              image={MerelPortrait.src}
-              quote="Research interests: Engineering AI-intensive Systems, IT Architecture, Quality in Software Engineering, Educating HBO ICT students"
-              personalPage="https://www.linkedin.com/in/merel-veracx-a806115/?originalSubdomain=be"
-            />
-            <PersonCard
-              name="Felipe Xavier"
-              position="PhD Candidate"
-              image={FelipePortrait.src}
-              quote="Research interests: Low-Code, Formal Verification, Embedded Systems, and Software architecture"
-              personalPage="https://research.tue.nl/en/persons/felipe-de-azeredo-coutinho-xavier"
-            />
-            <PersonCard
-              name="Kevin Bouwmeester"
-              position="PhD Candidate"
-              image={KevinPortrait.src}
-              quote="Research interests: Digital Twins, Software Architecture, Game-Design, and Software Design"
-              personalPage="https://research.tue.nl/en/persons/kevin-bouwmeester"
-            />
-            <PersonCard
-              name="Supun Wijayakoon"
-              position="PhD Candidate"
-              image={SupunPortrait.src}
-              quote="Research interests: Spftware Architecture, Software Analytics, Software Visualization"
-              personalPage="https://research.tue.nl/en/persons/supun-wijekoon-bandara-mudiyanselage"
-            />
+            <MichelCard />
+            <LinaCard />
+            <JacobCard />
+            <SatrioCard />
+            <FilipCard />
+            <MerelCard />
+            <FelipeCard />
+            <KevinCard />
+            <SupunCard />
           </div>
         </div>
       </main>
