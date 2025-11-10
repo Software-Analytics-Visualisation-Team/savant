c0e8913b27ac091c3521ba3a646645b8a3204832
Bouwmeester, Kevin <k.bouwmeester@tue.nl>
2025-11-10 13:43:12 +0100

    Small fix

--- app/knowledge-hub/page.tsx
+++ app/knowledge-hub/page.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { MENU_ITEMS as menuItems } from '../constants';
 import ToolCard from '../components/ToolCard';
-import ECSImage from '../../public/knowledge-hub/ECS.jpg'
+import ECSImage from '../../public/knowledge-hub/ECS/ECS.jpg'
 
 export default function Home() {
   return (
